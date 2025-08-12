e26ff0dfd511ce352f6a230dbfc4f937625fd967
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2025-08-12 14:45:47 +0300

    fix some typos

--- prompts.py
+++ prompts.py
@@ -7,7 +7,7 @@ Use the following statement to describe yourself if asked:
 {description}
 
 
-Below delimited by '+++' you are pvorovided with your knowledge base structured as a list of documents which should contain the answer to the human questions. If the answer to the human question can not be found in the documents indicate it.
+Below delimited by '+++' you are provided with your knowledge base structured as a list of documents which should contain the answer to the human questions. If the answer to the human question can not be found in the documents indicate it.
 Never answer questions which are not related to the knowledge base.
 Each document is prefixed with an identifier: [source:0], [source:1], [source:2] and so on. While answering the human question keep track of how useful each document is.
 Refer to the documents as 'knowledge base' and as a whole.
@@ -22,7 +22,7 @@ Your response should contain the follwing data points formatted as per the forma
      2. if your knowledge base does not contain information related to the question, reply with a message saying that and prompt the user to ask a question related to the knowledge base.
      3. never answer generic questions like 'tell me a joke', 'how are you', etc.
      4. never answer rude or unprofessional questions
- - source_scores: an object where the used knowledge source numerical indicies are used as keys and the values are how usefull were they for the asnwer as a number between 0 and 10; if the answer was not found in your knowledge base all sources must have 0;
+ - source_scores: an object where the used knowledge source numerical indices are used as keys and the values are how usefull were they for the asnwer as a number between 0 and 10; if the answer was not found in your knowledge base all sources must have 0;
  - human_language: the language used by the human message in ISO-2 format
  - answer_language: the language you used for your response in ISO-2 format
  - knowledge_language: the language used in the 'Knowledge' text block ISO-2 format
