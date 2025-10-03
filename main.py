2ff110b4c26b1add3ad3ca7f3ea83ae706c909cc
Vladimir Aleksiev <vladimiraleksiev@gmail.com>
2025-10-03 16:01:04 +0300

    fix typos

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ logger = setup_logger(__name__)
 
 logger.info(f"log level {os.path.basename(__file__)}: {LOG_LEVEL}")
 
+input_exclude = {}
+if LOG_LEVEL != "DEBUG":
+    input_exclude = {"prompt_graph"}
 
 async def on_request(input: Input) -> Response:
-    logger.info(f"Expert engine invoked; Input is {input.model_dump(exclude={'prompt_graph'})}")
+    logger.info(f"Expert engine invoked; Input is {input.model_dump(exclude=input_exclude)}")
     logger.info(
         f"AiPersonaServiceID={input.persona_id} with VC name `{input.display_name}` invoked."
     )
     result = await ai_adapter.invoke(input)
-    logger.info(f"LLM result: {result.mode_dump()}")
+    logger.info(f"LLM result: {result.model_dump()}")
     return result
 
 
